62f09dcd23946eb8d418a16a83d086585bed0e61
mehuljoshi1992 <mehul_joshi01@yahoo.com>
2021-01-24 15:47:04 -0600

    initial version

---
+++ jan24proj2/src/jan24proj2/Jan24proj2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jan24proj2;
+
+/**
+ *
+ * @author mehul1992
+ */
+public class Jan24proj2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello friend");
+        // TODO code application logic here
+    }
+    
+}
